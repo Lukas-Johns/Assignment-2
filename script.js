f062fe943d5bdca5a4e96901b123aa76afb018a1
LukasJohns <lukas.johns@ocsbstudent.ca>
2021-02-13 18:04:52 +0000

    fixing style lint errors

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.getElementById('DP').addEventListener('click', myButton)
-document.getElementById('UH').addEventListener('click', myImage)
-document.getElementById('VB').addEventListener('click', myDisplay)
-document.getElementById('GB').addEventListener('click', myPost)
-document.getElementById('GW').addEventListener('click', myFunction)
+document.getElementById('DP').addEventListener('click',  myButton)
+document.getElementById('UH').addEventListener('click',  myImage)
+document.getElementById('VB').addEventListener('click',  myDisplay)
+document.getElementById('GB').addEventListener('click',  myPost)
+document.getElementById('GW').addEventListener('click',  myFunction)
 function myButton() {
   document.getElementById('New').src = './images/D-pad.jpeg';
 }
