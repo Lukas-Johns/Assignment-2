cdfc37c1cece9c7f2e619f7e4f760a775432f5b1
LukasJohns <lukas.johns@ocsbstudent.ca>
2021-02-13 18:13:17 +0000

    Fixing linter errors part 2

--- script.js
+++ script.js
@@ -3,18 +3,23 @@ document.getElementById('UH').addEventListener('click',  myImage)
 document.getElementById('VB').addEventListener('click',  myDisplay)
 document.getElementById('GB').addEventListener('click',  myPost)
 document.getElementById('GW').addEventListener('click',  myFunction)
+
 function myButton() {
   document.getElementById('New').src = './images/D-pad.jpeg';
 }
+
 function myImage() {
   document.getElementById('New').src = './images/Ultra_Hand.png';
 }
+
 function myDisplay() {
   document.getElementById('New').src = './images/virtual_boy.jpeg';
 }
+
 function myPost() {
   document.getElementById('New').src = './images/Gameboy.jpeg';
 }
+
 function myFunction() {
-  document.getElementById('New').src = './images/Game_&_Watch.png'
+  document.getElementById('New').src = './images/Game_And_Watch.png'
 }
