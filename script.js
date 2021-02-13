5cd0db0805eea55f1d1fb821530cd92cef28bd6b
LukasJohns <lukas.johns@ocsbstudent.ca>
2021-02-13 18:27:36 +0000

    Linter part 3

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-document.getElementById('DP').addEventListener('click',  myButton)
-document.getElementById('UH').addEventListener('click',  myImage)
-document.getElementById('VB').addEventListener('click',  myDisplay)
-document.getElementById('GB').addEventListener('click',  myPost)
-document.getElementById('GW').addEventListener('click',  myFunction)
-
+document.getElementById('DP').addEventListener('click', myButton)
+document.getElementById('UH').addEventListener('click', myImage)
+document.getElementById('VB').addEventListener('click', myDisplay)
+document.getElementById('GB').addEventListener('click', myPost)
+document.getElementById('GW').addEventListener('click', myFunction)
 function myButton() {
   document.getElementById('New').src = './images/D-pad.jpeg';
 }
-
 function myImage() {
   document.getElementById('New').src = './images/Ultra_Hand.png';
 }
-
 function myDisplay() {
   document.getElementById('New').src = './images/virtual_boy.jpeg';
 }
-
 function myPost() {
   document.getElementById('New').src = './images/Gameboy.jpeg';
 }
-
 function myFunction() {
   document.getElementById('New').src = './images/Game_And_Watch.png'
 }
